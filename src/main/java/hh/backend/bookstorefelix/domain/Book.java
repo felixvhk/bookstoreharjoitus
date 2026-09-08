9eeebb4049effc6b95e79dac3ef582264079ba4e
Felix <felixvk@atomicmail.io>
2026-09-08 08:35:08 +0300

    päivitetty

--- src/main/java/hh/backend/bookstorefelix/domain/Book.java
+++ src/main/java/hh/backend/bookstorefelix/domain/Book.java
@@ -8,11 +8,31 @@ public class Book {
     private String isbn;
     private int price;
 
-    public Book(String title) {
+    public Book(String title, String author, int publicationYear, String isbn, int price) {
         this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
--- src/main/java/hh/backend/bookstorefelix/web/BookController.java
+++ src/main/java/hh/backend/bookstorefelix/web/BookController.java
@@ -1,9 +1,16 @@
 package hh.backend.bookstorefelix.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import hh.backend.bookstorefelix.domain.Book;
 
 
 @Controller
@@ -11,6 +18,18 @@ public class BookController {
 
     @RequestMapping("/index")
     public String showIndex() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+
+        List<Book> books = new ArrayList<>();
+
+        books.add(new Book("Very Cool Book", "Cole", 2020, "978-0-123456-78-9", 15));
+
+        model.addAttribute("books", books);
+
         return "index";
     }
     
